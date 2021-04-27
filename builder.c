f77165d073fa9d483dc85728533d059b515d01cb
Kindane <None>
2021-04-27 19:23:34 +0300

    refactored builder.c

--- builder.c
+++ builder.c
@@ -3,81 +3,60 @@
  */
 #include <stdio.h>
 #include <GL/glut.h>
-// TODO REFACTOR ALL CODE!!
+// ~~TODO REFACTOR ALL CODE!!~~ DONE
 
 void init();
 
 static const float size_of_square = 0.05;
 
 int main(int argc, char* argv[]) {
+    /* init main window */
     glutInit(&argc, argv);
     glEnable(GL_DEBUG_CALLBACK_FUNCTION);
     glutInitDisplayMode(GLUT_SINGLE);
     glutInitWindowSize(1280, 720);
     glutCreateWindow("Function Graphic Builder");
     glutDisplayFunc(init);
-    glutMainLoop();
+
+    glutMainLoop(); /* this is like a `return`, after this line code does now work */
     return 0;
 }
 
+
+
 void init() {
-    /* BEGIN OF THE NORMAL */
     glClear(GL_COLOR_BUFFER_BIT); /* clear color */
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f); /* bg color: can be from 0.0f to 1.0f */
 
-    glLineWidth(1);
-    glColor3f(0.0, 0.0, 0.0);
-    glBegin(GL_LINES);
-    /* horizontal lines */
-    int counter;
-    for (float i = 1; i >= -2; i -= size_of_square, counter++) {
-        /* abscissa */
-        if (counter == 20) {
-            glEnd();
-            glFlush();
-            glLineWidth(5);
-            glBegin(GL_LINES);
-            glEnable(GL_LINE_WIDTH);
-            glLineWidth(10);
-            glVertex2f(-1, i);
-            glVertex2f(1, i);
-            glLineWidth(1);
-            glEnd();
-            glFlush();
-            glLineWidth(1);
-            glBegin(GL_LINES);
-            glEnable(GL_LINE_WIDTH);
-            continue;
-        }
-        glVertex2f(-1, i);
-        glVertex2f(1, i);
-    }
-    counter = 0;
-    /* ordinate */
-    /* vertical lines */
+    glLineWidth(1); /* line with = 1 */
+    glColor3f(0.0, 0.0, 0.0); /* Line color: black */
+    glBegin(GL_LINES); /* Start draw! */
+
+    int counter = 0;
     for (float i = 1; i >= -2; i-= size_of_square, counter++) {
-        if (counter == 20) {
+        if (counter == 20) { /* Draw X and Y lines */
             glEnd();
             glFlush();
             glLineWidth(5);
             glBegin(GL_LINES);
-            glEnable(GL_LINE_WIDTH);
-            glLineWidth(10);
-            glVertex2f(i, -1);
-            glVertex2f(i, 1);
-            glLineWidth(1);
+
+            glVertex2f(i, -1); /* X */
+            glVertex2f(i, 1);  /* X */
+            glVertex2f(-1, i); /* Y */
+            glVertex2f(1, i);  /* Y */
+
             glEnd();
             glFlush();
             glLineWidth(1);
             glBegin(GL_LINES);
-            glEnable(GL_LINE_WIDTH);
             continue;
         }
         glVertex2f(i, -1);
         glVertex2f(i, 1);
+        glVertex2f(-1, i);
+        glVertex2f(1, i);
     }
 
     glEnd();
     glFlush();
-    /* END OF THE NORMAL */
 }
